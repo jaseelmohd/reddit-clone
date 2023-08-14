0c352eeb4de7f9d0f0d2628da810bab893758bbc
Jaseel Muhammad Kaithakkodan <jazeelmohd.kk@gmail.com>
2023-08-14 14:55:47 -0400

    Added model package

---
+++ backend/src/main/java/com/clone/redditclone/model/Comment.java
@@ -0,0 +1,30 @@
+package com.clone.redditclone.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    private Instant createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+}
